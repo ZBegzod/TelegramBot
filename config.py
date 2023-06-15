40c6ab4512ee28720edc3e1e8b3243b2be6e8e30
ZBegzod <zohidovbegzod4557@gmail.com>
2023-06-15 23:26:48 +0300

    configuration on for commands

--- config.py
+++ config.py
@@ -1,4 +1,19 @@
-import logging
 
-
+"""token"""
 API_TOKEN = '6150226885:AAEX9cBOEd-md5PZNk_B35Tu9LeastvlWTU'
+
+"""total commands"""
+HELP_COMMAND = """
+/<b>help</b>-list of command
+/<b>start</b>-start to work with bot 
+/<b>give</b>-take sticker by given id 
+/<b>photo</b>-send photo 
+"""
+
+"""sticker id"""
+sticker_id = "CAACAgIAAxkBAAEJRylkhXO8Cogamz5UbZwsmgzDYZTimwACIAkAAhhC7gjhiiCooToK2S8E"
+sticker_id2 = "CAACAgUAAxkBAAPwZIhRnGogNZedJHBE-skjGdyeavQAAnQHAAKZeWlXKJsSGTOCj2AvBA"
+
+"""photo url"""
+photo_link = "https://images.unsplash.com/photo-1622737133809-d95047b9e673?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8d2FsbHBhcGVyJTIwNGt8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60"
+girl_photo_id = "https://images.unsplash.com/photo-1550935114-99de2f488f47?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGJlYXV0aWZ1bCUyMGdpcmx8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60"
